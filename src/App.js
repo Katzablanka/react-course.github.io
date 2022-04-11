bfda6789de06f9fd730599bc7988cd96a0793ace
Katzablanka <72996614+Katzablanka@users.noreply.github.com>
2022-04-11 15:39:29 +0200

    sticky nav, content skroll

--- src/App.js
+++ src/App.js
@@ -8,19 +8,31 @@ import { Route, BrowserRouter } from 'react-router-dom';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
+import Container from '@mui/material/Container';
 
 const App = () => {
+  var docWidth = document.documentElement.offsetHeight;
+  [].forEach.call(document.querySelectorAll('*'), function (el) {
+    if (el.offsetHeight > docWidth) {
+      console.log('||');
+      console.log(el);
+      console.log('||');
+    }
+  });
   return (
     <BrowserRouter>
       <div className="app-wrapper">
         <Header />
         <Navbar />
+
         <div className="app-wrapper-content">
-          <Route component={Profile} path="/profile" />
-          <Route component={Dialogs} path="/messages" />
-          <Route component={News} path="/news" />
-          <Route component={Music} path="/music" />
-          <Route component={Settings} path="/settings" />
+          <div className="app-skroll-part">
+            <Route component={Profile} path="/profile" />
+            <Route component={Dialogs} path="/messages" />
+            <Route component={News} path="/news" />
+            <Route component={Music} path="/music" />
+            <Route component={Settings} path="/settings" />
+          </div>
         </div>
       </div>
     </BrowserRouter>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,29 +1,36 @@
-import React from 'react'
-import MyPosts from './MyPosts/MyPosts'
-import  s from './Profile.module.css'
+import React from 'react';
+import MyPosts from './MyPosts/MyPosts';
+import s from './Profile.module.css';
 
-const Profile=()=>{
-    return(
-     <div className={s.page_content}>
-       <section>
-         <div className={s.walpaper}>
-            <img className={s.ava} src="https://cs9.pikabu.ru/post_img/big/2016/11/26/12/1480192824129447047.jpg" alt="avatarka"/>
-            <img className={s.background} src="https://groknation.com/wp-content/uploads/2018/09/apertism-536752-unsplash.jpg" alt="walpaper of the user"/>
-         </div>
-       </section>
-        <section>
-          <div>
-            <section className={s.userInfo}>
-               <div>designer from San Francisco 23 y. o.</div>
-            </section>
-            <section>
-              <MyPosts/>
-            </section>
-         </div>
-        </section>
-      </div>
-    );
-
-}
+const Profile = () => {
+  return (
+    <div className={s.page_content}>
+      <section className={s.userInfo}>
+        <div className={s.walpaper}>
+          <img
+            className={s.ava}
+            src="https://cs9.pikabu.ru/post_img/big/2016/11/26/12/1480192824129447047.jpg"
+            alt="avatarka"
+          />
+          <img
+            className={s.background}
+            src="https://groknation.com/wp-content/uploads/2018/09/apertism-536752-unsplash.jpg"
+            alt="walpaper of the user"
+          />
+        </div>
+      </section>
+      <section>
+        <div>
+          <section className={s.userInfo}>
+            <div>designer from San Francisco 23 y. o.</div>
+          </section>
+          <section>
+            <MyPosts />
+          </section>
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Profile;
