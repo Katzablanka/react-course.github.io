47dc0d911f3a22e14cc86b5fcddb3120bf71bb3e
Katzablanka <72996614+Katzablanka@users.noreply.github.com>
2022-04-12 10:34:04 +0200

    chrome-bug fixed

--- src/components/Music/Music.jsx
+++ src/components/Music/Music.jsx
@@ -1,13 +1,8 @@
-import React from 'react'
-import s from './Music.module.css'
+import React from 'react';
+import s from './Music.module.css';
 
-const Music=(props)=>{
-    return(
-        <div className={s.color}>
-                Music
-        </div>
-    );
-}
-
+const Music = (props) => {
+  return <div className={s.color}>Music so was</div>;
+};
 
 export default Music;
