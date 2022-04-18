5e897013e4b9c363d324781e5dcdcec133d9da03
Katzablanka <72996614+Katzablanka@users.noreply.github.com>
2022-04-18 13:12:20 +0200

    navlink dialogs url

--- src/components/Dialogs/DialogsItem/DialogItem.jsx
+++ src/components/Dialogs/DialogsItem/DialogItem.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Grid, Paper, Box, Avatar, Typography } from '@mui/material';
 
+import { NavLink } from 'react-router-dom';
+
 const DialogItem = (props) => {
   return (
     <div>
-      <Grid container spacing={0.3} justifyContent="flex-start">
-        <Grid item xs={3}>
-          <Avatar src={props.ava} alt="photoURL" margin="0" />
+      <NavLink to={`/messages/${props.name}`}>
+        <Grid container spacing={0.3} justifyContent="flex-start">
+          <Grid item xs={3}>
+            <Avatar src={props.ava} alt="photoURL" margin="0" />
+          </Grid>
+          <Grid item xs={9}>
+            <Box>
+              <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
+                {props.name}
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                {props.status}
+              </Typography>
+            </Box>
+          </Grid>
         </Grid>
-        <Grid item xs={9}>
-          <Box>
-            <Typography variant="subtitle2" sx={{ color: 'text.primary' }}>
-              {props.name}
-            </Typography>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              {props.status}
-            </Typography>
-          </Box>
-        </Grid>
-      </Grid>
+      </NavLink>
     </div>
   );
 };
-
+// {`/messages/${props.name}`}
 export default DialogItem;
